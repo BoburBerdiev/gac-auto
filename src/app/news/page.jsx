1ba0bd887df990feeb70c4adc27f4e0d08b45307
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2024-07-05 19:23:28 +0500

    news page adding

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -1,4 +1,4 @@
-import {BannerSmall, ImgUI} from "@/components";
+import {BannerSmall, NewsCard} from "@/components";
 const bannerSlider = [
     {
         imgMob: '/news/news-baner.jpg',
@@ -8,6 +8,57 @@ const bannerSlider = [
 ]
 
 export default function Page(props) {
+    const newsCards = [
+        {
+            image: '/news/news-inner1.jpg',
+            date: "Jul 04,2022",
+            title: "GAC MOTOR｜COLO COLO",
+            href: '/news',
+            id: 0
+        },
+        {
+            image: '/news/news-inner2.jpg',
+            date: "Jun 20,2022",
+            title: "2022 GAC Design Award",
+            href: '/news',
+            id: 1
+        },
+        {
+            image: '/news/news-inner3.jpg',
+            date: "Aug 27,2021",
+            title: "GAC Owner's Story",
+            href: '/news',
+            id: 2
+        },
+        {
+            image: '/news/news-inner4.jpg',
+            date: "Aug 27,2021",
+            title: "Birth of a GAC MOTOR",
+            href: '/news',
+            id: 3
+        },
+        {
+            image: '/news/news-inner5.jpg',
+            date: "Jan 14,2019",
+            title: "2019 Nalas Debut: Entranze To The Future Mobility",
+            href: '/news',
+            id: 4
+        },
+        {
+            image: '/news/news-inner6.jpg',
+            date: "Jul 06,2018",
+            title: "Three River Source National Park",
+            href: '/news',
+            id: 5
+        },
+        {
+            image: '/news/news-inner7.jpg',
+            date: "Dec 29,2017",
+            title: "2018 North American International Auto Show",
+            href: '/news',
+            id: 6
+        },
+    ]
     return (
         <>
             <section>
@@ -15,22 +66,16 @@ export default function Page(props) {
             </section>
             <section className={'py-10 xl:py-[65px]'}>
                 <div className="container-fluid">
-                    <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 '}>
-                        <NewsCard/>
+                    <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 2xl:px-10'}>
+                        {
+                            newsCards?.map(newsCard => (
+                                <NewsCard title={newsCard?.title} href={newsCard?.href} image={newsCard?.image} date={newsCard?.date} key={newsCard?.id}/>
+                            ))
+                        }
                     </div>
                 </div>
             </section>
         </>
     );
 }
-
-const NewsCard = () => {
-    return (
-        <div className={'bg-white w-full h-fit '}>
-            <div className={'w-full aspect-[3/2] relative'}>
-                <ImgUI src={'/news/news-inner1.jpg'}/>
-            </div>
-        </div>
-    );
-};
 
---
+++ src/components/news-card.jsx
@@ -0,0 +1,19 @@
+import {ImgUI} from "@/components/index";
+import Link from "next/link";
+
+const NewsCard = ({image, date, title, href}) => {
+    return (
+        <Link href={href} className={'bg-white w-full h-fit hover:shadow-xl '}>
+            <div className={'w-full aspect-[3/2] xl:aspect-[7/4] relative'}>
+                <ImgUI src={image}/>
+            </div>
+            <div className={'p-4 border border-[#d2d2d2] xl:px-7 xl:pt-6 2xl:px-9 2xl:pt-8'}>
+                <span className={'w-10 h-0.5 bg-borderBtn block'}></span>
+                <p className={'pt-3 xl:pt-3.5 text-sm text-black mb-2'}>{date}</p>
+                <h3 className={'line-clamp-2 leading-[28px] h-14'}>{title}</h3>
+            </div>
+        </Link>
+    );
+};
+
+export default NewsCard;
