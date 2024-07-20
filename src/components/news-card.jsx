563e4e2fee0fa623a94729c2b3831dc1c68a9b9b
zokirjonovshohruh <shohruhfrontend@gmail.com>
2024-07-20 14:33:13 +0500

    pull ucun

--- src/components/news-card.jsx
+++ src/components/news-card.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 const NewsCard = ({image, date, title, href}) => {
     return (
-        <Link href={href} className={'bg-white w-full h-fit md:hover:shadow-xl '}>
-            <div className={'w-full aspect-[3/2] xl:aspect-[7/4] relative'}>
-                <ImgUI src={image}/>
+        <Link href={href} className={'bg-white w-full h-fit md:hover:shadow-xl group'}>
+            <div className={'w-full aspect-[3/2] xl:aspect-[7/4] overflow-hidden '}>
+                <div className="group-hover:scale-105 relative w=full h-full duration-150">
+                    <ImgUI src={image} />
+                </div>
             </div>
             <div className={'p-4 border border-[#d2d2d2] xl:px-7 xl:pt-6 2xl:px-9 2xl:pt-8'}>
                 <span className={'w-10 h-0.5 bg-borderBtn block'}></span>
