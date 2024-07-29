23d9b94d77017e3e59761302ba80c3ee84458118
zokirjonovshohruh <shohruhfrontend@gmail.com>
2024-07-29 19:04:31 +0500

    build

--- src/components/to-top.jsx
+++ src/components/to-top.jsx
@@ -20,6 +20,7 @@ const ToTop = () => {
             if (typeof window !== "undefined") {
                 window.addEventListener('scroll', handleScroll);
         }
+        
             // Clean up the event listener on component unmount
             return () => {
             if (typeof window !== "undefined") {
